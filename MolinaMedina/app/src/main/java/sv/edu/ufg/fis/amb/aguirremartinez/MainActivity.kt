540054c511312ddf9dad85fd1c35df2867898e31
T3kila <diegoalfmolina@gmail.com>
2024-03-01 21:39:35 -0600

    Subiendo guia

---
+++ MolinaMedina/app/src/main/java/sv/edu/ufg/fis/amb/aguirremartinez/MainActivity.kt
@@ -0,0 +1,26 @@
+package sv.edu.ufg.fis.amb.aguirremartinez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnEjecutar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnEjecutar = findViewById(R.id.btn_disparador)
+
+        btnEjecutar.setOnClickListener{
+            val Intent = Intent(this,ActivitySecundaria::class.java)
+            startActivity(Intent)
+        }
+
+
+    }
+}
